36293298b593965bb2b05592db7edafdec114026
JUles Takoulo <jules.takoulo.1@ulaval.ca>
2020-11-10 22:00:14 -0500

    initilaisation du projet

---
+++ main.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def analyser_commande():
+    # Créer un analyseur de ligne de commande
+    parser = argparse.ArgumentParser()
+    
+    # Insérer ici les bons appels à la méthode add_argument
+    
+    return parser.parse_args()
